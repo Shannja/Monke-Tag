8d5dabea8899988ee5211efe79abbeef14ce8ae5
Shannja <103908889+Shannja@users.noreply.github.com>
2024-02-24 15:35:06 -0500

    fixed version checking

--- Assets/Scripts/CheckNewVersion.cs
+++ Assets/Scripts/CheckNewVersion.cs
@@ -5,6 +5,7 @@ using UnityEngine.Networking;
 
 public class CheckNewVersion : MonoBehaviour
 {
+    string appVersion = "0.0.1";
     private void Awake()
     {
         StartCoroutine(CheckVersion());
@@ -12,10 +13,14 @@ public class CheckNewVersion : MonoBehaviour
 
     IEnumerator CheckVersion()
     {
-        UnityWebRequest www = UnityWebRequest.Get("https://raw.githubusercontent.com/Shannja/MonkeTag/master/VERSION");
+        UnityWebRequest www = UnityWebRequest.Get("https://raw.githubusercontent.com/Shannja/Monke-Tag/master/VERSION");
         yield return www.SendWebRequest();
         string onlineVersion = www.downloadHandler.text;
-
         Debug.Log(onlineVersion);
+
+        if (onlineVersion.CompareTo(appVersion) > 0)
+        {
+            Application.OpenURL("https://github.com/Shannja/Monke-Tag/releases");
+        }
     }
 }
